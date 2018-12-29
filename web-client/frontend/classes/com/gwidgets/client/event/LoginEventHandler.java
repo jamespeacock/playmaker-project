aef3f1fa98c42169924fceea7275705a5b87208e
jakepeacock <james.hoke.peacock@gmail.com>
2018-12-29 01:27:33 -0700

    remove frontend GWT

--- web-client/frontend/classes/com/gwidgets/client/event/LoginEventHandler.java
+++
@@ -1,8 +0,0 @@
-package com.gwidgets.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface LoginEventHandler extends EventHandler {
-	public void onLogin(LoginEvent event);
-
-}
--- web-client/frontend/gwt/devmode/work/com.gwidgets.MVPExample/compile-2/gen/com/google/gwt/lang/com_00046gwidgets_00046MVPExample__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class com_00046gwidgets_00046MVPExample__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
