4c57bda9e59637039c8b8c51b5ce8215a7bda931
jakepeacock <james.hoke.peacock@gmail.com>
2018-12-29 21:56:45 -0700

    Added redirect callback to grab auth code

--- playmaker/playmaker/login/urls.py
+++ playmaker/playmaker/login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SpotifyLoginView
+from .views import SpotifyLoginView, SpotifyCazllbackView
 
 urlpatterns = [
-    path('', SpotifyLoginView.as_view(), name="spotify-login")
+    path('', SpotifyLoginView.as_view(), name="spotify-login"),
+    path('get-auth', SpotifyCallbackView.as_view(), name="spotify-login-callback"),
 ]
--- playmaker/playmaker/login/views.py
+++ playmaker/playmaker/login/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 from api.settings import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI, SPOTIFY_SCOPE
 import spotipy
 import spotipy.util as util
+import spotipy.oauth2 as oauth2
 import logging
 import json
 import requests
@@ -13,7 +14,8 @@ import requests
 class SpotifyLoginView(LoginView):
 
     def get(self, request, *args, **kwargs):
-        username = kwargs.get('username', 'None')
+        # username = kwargs.get('username', 'None')
+        username = request.GET.get('username')
         logging.log(logging.INFO, "In spotify login GET %s" % username)
 
         # Create a Spotify client that can access my saved song information.
@@ -25,15 +27,19 @@ class SpotifyLoginView(LoginView):
         url = 'https://accounts.spotify.com/authorize?username=%s?scope=%s?client_id=%s?client_secret=%s?redirect_uri=%s' % (username, SPOTIFY_SCOPE, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI)
 
         return redirect(url)
-        # print(response)
-        # response_data = json.loads(response.text)
-        # access_token = response_data["access_token"]
-        # refresh_token = response_data["refresh_token"]
-        # token_type = response_data["token_type"]
-        # expires_in = response_data["expires_in"]
-        #
-        # logging.log(logging.WARN, expires_in)
 
+class SpotifyCallbackView():
 
-        # return HttpResponse(status=200)
+    def get(self, request, *args, **kwargs):
+        print(request.url)
+        auth_code = request.url.split("?code=")[1].split("&")[0]
 
+        sp_oauth = oauth2.SpotifyOAuth(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, 'unused_redirect_url',
+                                       scope=SPOTIFY_SCOPE)
+
+        token_info = sp_oauth.get_access_token(auth_code)
+
+        #Get user based on username, save token info to User object, return success.
+        #Now backend has credentials for logged in user
+
+        return HttpResponse(status=200, data=token_info)
