67a075dec2e62432b91fdc53ee9d1a4dec84dab3
jakepeacock <james.hoke.peacock@gmail.com>
2019-08-05 22:17:14 -0400

    switch to port 5000

--- playmaker/start.sh
+++ playmaker/start.sh
@@ -6,5 +6,5 @@ exec python3.6 /app/manage.py collectstatic
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn api.wsgi:application \
-    --bind 0.0.0.0:8000 \
+    --bind 0.0.0.0:5000 \
     --workers 3
