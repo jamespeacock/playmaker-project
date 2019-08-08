20320746f149af8ba9710690c8971aea0926be77
jakepeacock <james.hoke.peacock@gmail.com>
2019-08-07 21:54:42 -0400

    Adding nginx

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
-docker-compose down && docker-compose up -d
+echo $1
+if [ "$1" = "dev" ]; then
+    echo "Refreshing local"
+    docker-compose down && docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+else
+    echo "Refreshing production"
+    docker-compose down && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+fi
