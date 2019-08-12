102bec1c44fe2d1f7de43a0915bedc7f4bc5fd30
jakepeacock <james.hoke.peacock@gmail.com>
2019-08-11 20:21:23 -0400

    production frontend

--- playmaker/playmkr-ui/src/API/ApiInterface.js
+++ playmaker/playmkr-ui/src/API/ApiInterface.js
@@ -2,6 +2,8 @@ import config from '../config'
 import axios from 'axios';
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.withCredentials = true;
+
 
 export default class ApiInterface  {
     
