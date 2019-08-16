c050ac8bdb5f789d728098034afaa8cb9a48995b
jakepeacock <james.hoke.peacock@gmail.com>
2019-08-16 15:39:39 -0400

    back to combined frontend

--- playmaker/api/urls.py
+++ playmaker/api/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('/', admin.site.urls),
+    path('/admin/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('login/', include('playmaker.login.urls')),
     path('controller/', include('playmaker.controller.urls')),
