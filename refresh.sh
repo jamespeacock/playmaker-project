f29cd40787fadedd5e72f8d47b764b7b3b92f952
jakepeacock <james.hoke.peacock@gmail.com>
2019-11-09 20:48:32 -0500

    Update nginx to get production requests to backend working

--- refresh.sh
+++ refresh.sh
@@ -5,7 +5,7 @@ if [ "$1" = "dev" ]; then
     docker-compose down && docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
 else
     echo "Refreshing production"
-    if [ "$1" = "build" ]; then
+    if [ "$2" = "build" ]; then
         docker-compose down && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
         docker exec -it interface python3.6 manage.py migrate
     else
