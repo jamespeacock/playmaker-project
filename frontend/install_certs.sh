f76770eb0c8130c9d0e8201ecd699965984c7fc6
jakepeacock <james.hoke.peacock@gmail.com>
2019-11-11 21:05:53 -0500

    Add install_certs

--- frontend/install_certs.sh
+++ frontend/install_certs.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #export D=/usr/share/nginx/html
-#export SITE=test.myplaymkr.co
+#export SITE=test
+#export DOMAIN=myplaymkr.co
 openssl dhparam -dsaparam -out dhparams.pem 4096
-cd /root/.acme.sh && ./acme.sh --issue -w $D -d ${SITE} -k 4096
-cd /root/.acme.sh && ./acme.sh --installcert -d ${SITE} \
---keypath /etc/nginx/ssl/letsencrypt/${SITE}/${SITE}.key \
---fullchainpath /etc/nginx/ssl/letsencrypt/${SITE}/${SITE}.crt
+cd /root/.acme.sh && ./acme.sh --issue -w $D -d ${SITE}.${DOMAIN} -k 4096
+cd /root/.acme.sh && ./acme.sh --installcert -d ${SITE}.${DOMAIN} \
+--keypath /etc/nginx/ssl/letsencrypt/${DOMAIN}/${DOMAIN}.key \
+--fullchainpath /etc/nginx/ssl/letsencrypt/${DOMAIN}/${DOMAIN}.crt
