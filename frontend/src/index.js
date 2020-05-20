b170ed83910f88e64658ae0e9fe92760c8c56cce
root <root@myplaymkr.co>
2020-05-20 02:12:30 +0000

    css fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React  from 'react';
 import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom';
-import './theme.css';
+import './theme.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Root from './Root';
 
